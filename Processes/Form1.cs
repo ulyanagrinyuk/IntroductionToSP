9065c78490ef0e7642612a0353167d89bf585f09
Uliana.29 <Uliana.29@DESKTOP-6DD5K9D>
2024-02-12 16:47:49 +0300

    Operation with multiple Processes

--- Processes/Form1.cs
+++ Processes/Form1.cs
@@ -8,16 +8,19 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace Processes
 {
 	public partial class Form1 : Form
 	{
+		List<Process> process_list;
 		public Form1()
 		{
 			InitializeComponent();
+			process_list = new List<Process>();
 			richTextBoxProcessName.Text = "notepad";
-			InitProcess();
+			//InitProcess();
 		}
 		//void Form1_Closing(object sender, CancelEventArgs e)
 		//{
@@ -27,7 +30,11 @@ namespace Processes
 		void InitProcess()
 		{
 			AllignText();
+			myProcess = new Process();
 			myProcess.StartInfo = new System.Diagnostics.ProcessStartInfo(richTextBoxProcessName.Text);
+			myProcess.Start();
+			//myProcess = new System.Diagnostics.Process(richTextBoxProcessName.Text);
+			process_list.Add(myProcess);
 		}
 		void AllignText()
 		{
@@ -38,38 +45,72 @@ namespace Processes
 		private void buttonStart_Click(object sender, EventArgs e)
 		{
 			InitProcess();
-			myProcess.Start();
+			//myProcess.Start();
 			Info();
+			//this.TopMost = true;
 		}
 
 		private void buttonStop_Click(object sender, EventArgs e)
 		{
-			
-			myProcess.CloseMainWindow();			  //закрывает процесс
-			myProcess.Close();						 //освобождает ресурсы, занимаемые процессом
-			Environment.Exit(Environment.ExitCode);  //Завершает процесс и возвращает код выхода операционной системе.
+			if (process_list.Count > 0)
+			{
+				try
+				{
+					myProcess = process_list.Last();
+					myProcess.CloseMainWindow();              //закрывает процесс
+					myProcess.Close();                       //освобождает ресурсы, занимаемые процессом
+					process_list.RemoveAt(process_list.Count - 1);
+
+				}
+				catch (Exception ex)
+				{
+					process_list.RemoveAt(0);
+					Info();
+				}
+			}
 		}
 
 		private void Form1_FormClosing(object sender, FormClosingEventArgs e)
 		{
-			myProcess.CloseMainWindow();
-			myProcess.Close();
+
+			while(process_list.Count >0)
+			{
+				try
+				{
+					process_list.First().CloseMainWindow();
+					process_list.Last().CloseMainWindow();
+					process_list.RemoveAt(0);
+				}
+				catch(Exception ex)
+				{
+					process_list.RemoveAt(0);
+				}
+			}
 		}
 
 		void Info()
 		{
-			labelProcessInfo.Text = "Process info:\n";
-			labelProcessInfo.Text += $"PID:				{myProcess.Id}\n";
-			labelProcessInfo.Text += $"Base priority:	{myProcess.BasePriority}\n";
-			labelProcessInfo.Text += $"Priority class:	{myProcess.PriorityClass}\n";
-			labelProcessInfo.Text += $"Start time:		{myProcess.StartTime}\n";
-			labelProcessInfo.Text += $"Total CPU time:	{myProcess.TotalProcessorTime}\n";
-			labelProcessInfo.Text += $"User  CPU time:	{myProcess.UserProcessorTime}\n";
-			labelProcessInfo.Text += $"User  CPU time:	{myProcess.UserProcessorTime}\n";
-			labelProcessInfo.Text += $"Session ID:		{myProcess.SessionId}\n";
-			labelProcessInfo.Text += $"Name:			{myProcess.ProcessName}\n";
-			labelProcessInfo.Text += $"Affinity:		{myProcess.ProcessorAffinity}\n";
-			labelProcessInfo.Text += $"Threads:			{myProcess.Threads.Count}\n";
+			if (process_list.Count > 0)
+			{
+			myProcess = process_list.First();
+				labelProcessInfo.Text = $"Total process count: {process_list.Count}";
+				labelProcessInfo.Text += $"PID:				{myProcess.Id}\n";
+				labelProcessInfo.Text += $"Base priority:	{myProcess.BasePriority}\n";
+				labelProcessInfo.Text += $"Priority class:	{myProcess.PriorityClass}\n";
+				labelProcessInfo.Text += $"Start time:		{myProcess.StartTime}\n";
+				labelProcessInfo.Text += $"Total CPU time:	{myProcess.TotalProcessorTime}\n";
+				labelProcessInfo.Text += $"User  CPU time:	{myProcess.UserProcessorTime}\n";
+				labelProcessInfo.Text += $"User  CPU time:	{myProcess.UserProcessorTime}\n";
+				labelProcessInfo.Text += $"Session ID:		{myProcess.SessionId}\n";
+				labelProcessInfo.Text += $"Name:			{myProcess.ProcessName}\n";
+				labelProcessInfo.Text += $"Affinity:		{myProcess.ProcessorAffinity}\n";
+				labelProcessInfo.Text += $"Threads:			{myProcess.Threads.Count}\n";
+			}
+			else labelProcessInfo.Text = "Нет запущенных процессов";
+		}
+		void RemoveLost()
+		{
+
 		}
 	}
 }
