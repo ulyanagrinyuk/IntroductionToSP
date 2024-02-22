de31a11e9d38d15a1a04a983f72ae3b13aee3d24
Uliana.29 <Uliana.29@DESKTOP-6DD5K9D>
2024-02-22 18:01:12 +0300

    Encoding

---
+++ Domains/AppDomainDynamicUnload/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace AppDomainDynamicUnload
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			//AppDomain dome = AppDomain.CreateDomain("Demo");
+			//Assembly asm = domain.Load(AssemblyName.GetAssemblyName("SampleLibrary.dll"));
+			//Module module = asm.GetModule("SampleLibrary.dll");
+			//Type type = module.GetType("SampleLibrary.SampleClass");
+			//MethodInfo method = type.GetMethod("Print");
+
+		}
+	}
+}
---
+++ Domains/TextWindow/MainForm.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TextWindow
+{
+	public partial class MainForm : System.Windows.Forms.Form
+	{
+		Module DrawModule {  get; set; }
+		object drawer;
+		
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+		public MainForm(Module module, object targetWindow) : this()
+		{
+			InitializeComponent();
+			DrawModule = module;
+			drawer = targetWindow;
+		}
+
+		private void textBox1_TextChanged(object sender, EventArgs e)
+		{
+			DrawModule.GetType("TextdRAWER.MainForm").GetMethod("SetText").Invoke(drawer, new object[] {textBox1.Text});
+		}
+
+		private void MainForm_LocationChanged(object sender, EventArgs e)
+		{
+			DrawModule.GetType("TextDrawer.MainForm").GetMethod("Move").Invoke
+				(
+				drawer, 
+				new object[] 
+				{
+					new Point(this.Location.X, this.Location.Y + this.Height),
+					this.Width
+				}
+				);
+		}
+	}
+}
